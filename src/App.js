d60838579e9965f27f2f2dc90420d10108dbf3b2
shrenik49 <41732400+shrenik49@users.noreply.github.com>
2022-07-05 22:50:13 +0530

    delete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import "./styles.css";
-// import Join from '../component/Join'
 
 function Books({ arr }) {
   console.log(arr); // 👉️ ['A', 'B', 'C', 'D']
   return (
     <div>
       <h1>{arr.join()}</h1>
-      {/* {arr.map((title) => {
-        return <div key={title}>{title}</div>;
-      })} */}
     </div>
   );
 }
